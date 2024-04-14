ea879b79e2e0bcc65137ecc4fb7f26de5ad69a01
Saravanan <saravananr0104@gmail.com>
2024-04-14 18:27:32 +0530

    made models

--- all/models.py
+++ all/models.py
@@ -1,4 +1,102 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 
+class Company(models.Model):
+    c_id=models.CharField(primary_key=True,max_length=5)
+    c_name=models.CharField(max_length=30,null=False)
+    c_phone=models.IntegerField(null=False)
+    c_address=models.CharField(max_length=200,null=False)
+    c_email=models.CharField(null=False,max_length=30)
+    c_password=models.CharField(null=False,max_length=30)
+
+
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super(Company, self).save(*args, **kwargs)
+
+class Branch(models.Model):
+    b_id=models.CharField(primary_key=True,max_length=5)
+    c_id=models.ForeignKey(Company, on_delete=models.CASCADE)
+    b_address=models.CharField(null=False,max_length=200)
+    b_phone_no=models.IntegerField(null=False)
+
+class Warehouse(models.Model):
+    w_id=models.CharField(primary_key=True,max_length=5)
+    b_id=models.ForeignKey(Branch,on_delete=models.CASCADE)
+    w_admin_id=models.CharField(null=True,max_length=5)
+
+class Store(models.Model):
+    s_id=models.CharField(primary_key=True,max_length=5)
+    b_id=models.ForeignKey(Branch,on_delete=models.CASCADE)
+    s_admin_id=models.CharField(null=True,max_length=5)
+
+class W_Products(models.Model):
+    p_id=models.CharField(primary_key=True,max_length=5)
+    w_id=models.ForeignKey(Warehouse,on_delete=models.CASCADE)
+    p_name=models.CharField(null=False,max_length=30)
+    p_manufacturer=models.CharField(max_length=30,null=False)
+    price=models.IntegerField(null=False)
+    mfg_date=models.DateField(null=False)
+    expiry_date=models.DateField(null=False)
+    quantity=models.IntegerField(null=False)
+
+class S_Products(models.Model):
+    p_id=models.CharField(primary_key=True,max_length=5)
+    s_id=models.ForeignKey(Store,on_delete=models.CASCADE)
+    p_name=models.CharField(null=False,max_length=30)
+    p_manufacturer=models.CharField(max_length=30,null=False)
+    price=models.IntegerField(null=False)
+    mfg_date=models.DateField(null=False)
+    expiry_date=models.DateField(null=False)
+    quantity=models.IntegerField(null=False)
+
+class Employee(models.Model):
+    emp_id=models.CharField(primary_key=True,max_length=5)
+    b_id=models.ForeignKey(Branch,on_delete=models.CASCADE)
+    emp_name=models.CharField(max_length=40,null=False)
+    emp_phone_no=models.IntegerField(null=False)
+    # department=models.CharField()
+    dob=models.DateField(null=False)
+    salary=models.IntegerField(null=False)
+    emp_email=models.CharField(null=False,max_length=30)
+    emp_password=models.CharField(null=False,max_length=30)
+
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super(Employee, self).save(*args, **kwargs)
+
+# class Departments(models.Model):
+#     dept_id=models.CharField(primary_key=True)
+#     b_id=models.ForeignKey(Branch,on_delete=models.CASCADE)
+#     d_head_id=models.ForeignKey(Employee,on_delete=models.CASCADE)
+#     d_name=models.CharField(null=False)
+#     d_size=models.IntegerField()
+
+class Customers(models.Model):
+    cus_id=models.CharField(primary_key=True,max_length=5)
+    cus_name=models.CharField(max_length=30)
+    cus_join_date=models.DateField(null=False)
+    cus_points=models.IntegerField(null=False)
+    dob= models.DateField(null=False)
+    cus_phone_no=models.IntegerField()
+    cus_email=models.CharField(null=False,max_length=30)
+    cus_password=models.CharField(null=False,max_length=30)
+
+    
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super(Customers, self).save(*args, **kwargs)
+
+class Transaction(models.Model):
+    t_id=models.CharField(primary_key=True,max_length=5)
+    emp_id=models.ForeignKey(Employee,on_delete=models.CASCADE)
+    date_of_transaction=models.DateField(null=False)
+    cus_id=models.ForeignKey(Customers,on_delete=models.CASCADE)
+
+class Orders(models.Model):
+    o_id=models.CharField(primary_key=True,max_length=5)
+    t_id=models.ForeignKey(Transaction,on_delete=models.CASCADE)
+    p_id=models.ForeignKey(S_Products,on_delete=models.CASCADE)
+    quantity=models.IntegerField(null=False)
