a8e8d00a3520527a75f4d43824090a04a6b729e5
Saravanan <saravananr0104@gmail.com>
2024-04-14 19:45:31 +0530

    convert jquery to django templating

---
+++ remag/all/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("logincompany", views.company_login, name="logincompany"),
+    path("loginemp", views.emp_login, name="loginemp"),
+    path("logincus", views.cus_login, name="logincus"),
+    path("signupcompany", views.company_signup, name="signupcompany"),
+    path("signupcus", views.company_login, name="signupcus"),
+    path("companypage", views.company_page, name="companypage"),
+]
---
+++ remag/all/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+
+
+def home(request):
+    return render(request, "all/home.html",{})
+
+def company_login(request):
+    return render(request, "all/login-company.html",{})
+
+def emp_login(request):
+    return render(request, "all/login-employee.html",{})
+
+def cus_login(request):
+    return render(request, "all/login-customer.html",{})
+
+def company_signup(request):
+    return render(request, "all/signup-company.html",{})
+
+def cus_signup(request):
+    return render(request, "all/signup-customer.html",{})
+
+def company_page(request):
+    return render(request,"all/company-page.html",{})
+
+def branch_page(request):
+    return render(request,"all/edit-branch.html",{})
