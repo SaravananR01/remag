21378475bab7235b2cd859f4d2e5e3a187f120c2
Saravanan <saravananr0104@gmail.com>
2024-04-14 20:23:32 +0530

    render web pages

--- all/views.py
+++ all/views.py
@@ -19,3 +19,40 @@ def company_signup(request):
 
 def cus_signup(request):
     return render(request, "all/signup-customer.html",{})
+
+def company_page(request):
+    return render(request,"all/company-page.html",{})
+
+def branch_page(request):
+    return render(request,"all/edit-branch.html",{})
+
+def edit_emp(request):
+    return render(request,"all/edit-employee.html",{})
+
+def edit_shop(request):
+    return render(request,"all/edit-shop-warehouse.html",{})
+
+def emp_page(request):
+    return render(request,"all/employee-page.html",{})
+
+def modify_item_details(request):
+    return render(request,"all/modify_tem_details.html",{})
+
+def modify_stock(request):
+    return render(request,"all/modify_stock.html",{})
+
+def transaction_details(request):
+    return render(request,"all/transaction_details.html",{})
+
+def transaction(request):
+    return render(request,"all/transaction.html",{})
+
+def add_item(request):
+    return render(request,"all/add_itm.html",{})
+
+def source_items(request):
+    return render(request,"all/source_items.html",{})
+
+def customerpage(request):
+    return render(request,"all/customer-page.html",{})
+
