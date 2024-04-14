b307dd4dbf2d4ee7e552ccef5662f07dab556927
Topkinsme <s.aditya49chn@gmail.com>
2024-04-14 19:19:10 +0530

    Set up views and routes for the login pages, made them redirect to each other as well.

--- all/urls.py
+++ all/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("logincompany", views.company_login, name="logincompany"),
+    path("loginemp", views.emp_login, name="loginemp"),
+    path("logincus", views.cus_login, name="logincus"),
+    path("signupcompany", views.company_signup, name="signupcompany"),
+    path("signupcus", views.company_login, name="signupcus"),
 ]
--- all/views.py
+++ all/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "all/home.html",{})
+
+def company_login(request):
+    return render(request, "all/login-company.html",{})
+
+def emp_login(request):
+    return render(request, "all/login-employee.html",{})
+
+def cus_login(request):
+    return render(request, "all/login-customer.html",{})
+
+def company_signup(request):
+    return render(request, "all/signup-company.html",{})
+
+def cus_signup(request):
+    return render(request, "all/signup-customer.html",{})
